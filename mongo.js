258e545a97e82f61570db79eb130052aa50b6ae1
Domonkos Lezsák <lezsakdomi1@gmail.com>
2018-09-08 23:29:56 +0200

    Rename mongo env vars

--- mongo.js
+++ mongo.js
@@ -3,8 +3,8 @@ const Mongo = require('mongodb')
 const process = require('process')
 const debug = require('debug')('kronologia:backend:mongo')
 
-const url = process.env.MONGO_URL || 'mongodb://localhost:27017' //TODO config
-const db = process.env.MONGO_DB || 'kronologia' //TODO config
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017' //TODO config
+const db = process.env.MONGODB_DB || 'kronologia' //TODO config
 
 module.exports = new Promise((resolve, reject) => Mongo.MongoClient.connect(url, (err, client) => {
 	if (err) {
