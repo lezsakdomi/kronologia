2ea8da5c44255ff37a03e795cbd3e388d4a6d3a8
Domonkos Lezsák <lezsakdomi1@gmail.com>
2018-09-09 11:46:07 +0200

    Fix MongoDB configuration

--- mongo.js
+++ mongo.js
@@ -1,18 +1,25 @@
 const Mongo = require('mongodb')
 // noinspection NpmUsedModulesInstalled
 const process = require('process')
+const config = require('config')
 const debug = require('debug')('kronologia:backend:mongo')
 
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017' //TODO config
-const db = process.env.MONGODB_DB || 'kronologia' //TODO config
+const mongoClient = new Mongo.MongoClient(config.get('mongo.url'), config.get('mongo.options'))
 
-module.exports = new Promise((resolve, reject) => Mongo.MongoClient.connect(url, (err, client) => {
+module.exports = new Promise((resolve, reject) => mongoClient.connect((err, client) => {
 	if (err) {
 		debug('MongoDB connection failed')
 		reject(err)
 	} else {
 		debug('Connection succeed')
+
+		let db
+		if (config.has('mongo.db')) {
+			db = config.get('mongo.db')
+		}
+
 		resolve(client.db(db))
+		debug('Selected database %o', db || client.s.options.dbName)
 	}
 }))
 
