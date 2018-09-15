8b2b77e093e102af59200e4d0127749a0a1575c7
Lezsák Domonkos <lezsakdomi1@gmail.com>
2018-09-15 15:07:09 +0200

    Fix `new` button click (PR #18)
    * newQuiz function
    
    * Assign click for new button
    
    * Create `postJSON` and use it in `updateData` and `newQuiz`

--- public/quiz-form.js
+++ public/quiz-form.js
@@ -123,6 +123,8 @@ function registerHandlers(document) {
 
 	customClick('form[action="update"] input[type=submit]', () => updateData(), false)
 
+	customClick('form[action="new"] input[type=submit]', () => newQuiz(undefined, true))
+
 	function customClick(selector, handler, required = true) {
 		handleEvent(selector, 'click', function (evt) {
 			try {
@@ -313,18 +315,7 @@ function gatherData() {
 }
 
 function updateData(data = gatherData(), reload = false) {
-	const promise = fetch("update", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json; charset=utf-8"
-		},
-		body: JSON.stringify(data),
-	})
-		.then(response => response.json())
-		.then(response => {
-			assert(response.ok)
-			return response
-		})
+	const promise = postJSON("update", data)
 
 	promise.then(() => {
 		window.remoteData = data
@@ -342,6 +333,29 @@ function updateData(data = gatherData(), reload = false) {
 				}
 			}
 			return location.reload()
+		})
+	}
+}
+
+function newQuiz(data = gatherData(), redirect = false) {
+	const promise = postJSON("new", data)
+
+	promise.then(() => {
+		window.remoteData = data
+	}, e => {
+		console.error(e)
+		if (reload) alert("Failed to save data")
+	})
+
+	if (redirect) {
+		promise.then(() => {
+			if (window.onbeforeunload) {
+				const originalOnbeforeunload = window.onbeforeunload
+				window.onbeforeunload = function () {
+					originalOnbeforeunload.apply(this, arguments)
+				}
+			}
+			return window.location = 'index.html'
 		})
 	}
 }
@@ -480,6 +494,21 @@ function removeTr(tr_or_eid) {
 	}
 
 	tr_or_eid.parentElement.removeChild(tr_or_eid)
+}
+
+function postJSON(url, data) {
+	return fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json; charset=utf-8"
+		},
+		body: JSON.stringify(data),
+	})
+		.then(response => response.json())
+		.then(response => {
+			assert(response.ok)
+			return response
+		})
 }
 
 function fs(...fa) {
